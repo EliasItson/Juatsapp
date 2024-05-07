9794728090eb76756340c5d59ce7ad1214e6a0b9
GitHElias <jorge.elias@potros.itson.edu.mx>
2024-05-06 21:05:50 -0700

    Update POJOs

--- Juatsapp/src/main/java/modelo/Chat.java
+++ Juatsapp/src/main/java/modelo/Chat.java
@@ -1,13 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Ryzen 5
- */
-public class Chat {
+import java.time.LocalTime;
+import org.bson.types.ObjectId;
+
+
+public class Chat 
+{
+    private ObjectId id;
+    private ObjectId emisorId;
+    private ObjectId receptorId;
+    private String contenido;
+    private LocalTime fechaTiempoEnvio;
+
+    public Chat(ObjectId emisorId, ObjectId receptorId, String contenido, LocalTime fechaTiempoEnvio) 
+    {
+        this.emisorId = emisorId;
+        this.receptorId = receptorId;
+        this.contenido = contenido;
+        this.fechaTiempoEnvio = fechaTiempoEnvio;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public ObjectId getEmisorId() {
+        return emisorId;
+    }
+
+    public void setEmisorId(ObjectId emisorId) {
+        this.emisorId = emisorId;
+    }
+
+    public ObjectId getReceptorId() {
+        return receptorId;
+    }
+
+    public void setReceptorId(ObjectId receptorId) {
+        this.receptorId = receptorId;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public LocalTime getFechaTiempoEnvio() {
+        return fechaTiempoEnvio;
+    }
+
+    public void setFechaTiempoEnvio(LocalTime fechaTiempoEnvio) {
+        this.fechaTiempoEnvio = fechaTiempoEnvio;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "Chat{" + "emisorId=" + emisorId + ", receptorId=" + receptorId + ", contenido=" + contenido + ", fechaTiempoEnvio=" + fechaTiempoEnvio + '}';
+    }
+    
+    
+    
     
 }
--- Juatsapp/src/main/java/modelo/Mensaje.java
+++ Juatsapp/src/main/java/modelo/Mensaje.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Ryzen 5
- */
-public class Mensaje {
+import java.util.List;
+import org.bson.types.ObjectId;
+
+
+public class Mensaje 
+{
+    private ObjectId id;
+    private List<Usuario> miembros;
+    private List<Mensaje> mensajes;
+
+    public Mensaje(List<Usuario> miembros, List<Mensaje> mensajes) 
+    {
+        this.miembros = miembros;
+        this.mensajes = mensajes;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public List<Usuario> getMiembros() {
+        return miembros;
+    }
+
+    public void setMiembros(List<Usuario> miembros) {
+        this.miembros = miembros;
+    }
+
+    public List<Mensaje> getMensajes() {
+        return mensajes;
+    }
+
+    public void setMensajes(List<Mensaje> mensajes) {
+        this.mensajes = mensajes;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "Mensaje{" + "miembros=" + miembros + ", mensajes=" + mensajes + '}';
+    }
+    
     
 }
