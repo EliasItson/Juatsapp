63f6ca5e9d3cb88fb50fc7c38e59ad17b3b3aa0f
GitHElias <jorge.elias@potros.itson.edu.mx>
2024-05-09 19:28:25 -0700

    fixed loading images

--- JuatsApp/src/main/java/com/bda/juatsapp/ChatController.java
+++ JuatsApp/src/main/java/com/bda/juatsapp/ChatController.java
@@ -5,25 +5,30 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 
-public class ChatController implements Initializable {
+public class ChatController implements Initializable 
+{
 
     @FXML
     private Circle userPP;
+    private Label userDisplayName;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb) 
+    {
 
-        URL imageUrl = getClass().getClassLoader().getResource("media/yata.jpg");
+        URL imageUrl = getClass().getResource("/media/vera.png");
         if (imageUrl != null) {
             Image img = new Image(imageUrl.toExternalForm());
             userPP.setFill(new ImagePattern(img));
         } else {
             System.out.println("Image resource not found.");
         }
-
+        
+        userDisplayName.setText("Jorge Elias");
     }
 }
