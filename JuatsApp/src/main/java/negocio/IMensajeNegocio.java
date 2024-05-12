6f0f28b714e671c696e6ee82430c54858b83dd4e
GitHElias <jorge.elias@potros.itson.edu.mx>
2024-05-11 19:10:10 -0700

    Create login.fxml

---
+++ JuatsApp/src/main/java/negocio/IMensajeNegocio.java
@@ -0,0 +1,14 @@
+package negocio;
+
+import java.util.List;
+import modelo.Mensaje;
+import org.bson.types.ObjectId;
+import persistencia.PersistenciaException;
+
+
+public interface IMensajeNegocio 
+{
+    public List<Mensaje> getAllMensajes() throws NegocioException;
+    public List<Mensaje> getMensajesByEmisor(ObjectId emisor) throws NegocioException;
+    public void guardar(Mensaje mensaje) throws NegocioException;
+}
---
+++ JuatsApp/src/main/java/negocio/MensajeNegocio.java
@@ -0,0 +1,83 @@
+package negocio;
+
+import java.util.List;
+import modelo.Mensaje;
+import org.bson.types.ObjectId;
+import persistencia.MensajeDAO;
+import persistencia.PersistenciaException;
+
+public class MensajeNegocio implements IMensajeNegocio
+{
+    MensajeDAO mensajeDAO;
+    
+    public MensajeNegocio(MensajeDAO mensajeDAO)
+    {
+        this.mensajeDAO = mensajeDAO;
+    }
+    
+    @Override
+    public List<Mensaje> getAllMensajes() throws NegocioException
+    {
+        try 
+        {
+            List<Mensaje> mensajes = this.mensajeDAO.getAllMensajes();
+            if (mensajes == null) 
+            {
+                throw new NegocioException("No existen mensajes registrados");
+            }
+
+            return mensajes;
+        } 
+        catch (PersistenciaException e) 
+        {
+            System.out.println(e.getMessage());
+            throw new NegocioException(e.getMessage());
+        }
+    }
+    
+    @Override
+    public List<Mensaje> getMensajesByEmisor(ObjectId emisor) throws NegocioException
+    {
+        try 
+        {
+            List<Mensaje> mensajes = this.mensajeDAO.getMensajesByEmisor(emisor);
+            if (mensajes == null) 
+            {
+                throw new NegocioException("No existen mensajes registrados");
+            }
+
+            return mensajes;
+        } 
+        catch (PersistenciaException e) 
+        {
+            System.out.println(e.getMessage());
+            throw new NegocioException(e.getMessage());
+        }
+    }
+    
+    @Override
+    public void guardar(Mensaje mensaje) throws NegocioException
+    {
+        try 
+        {
+            if (mensaje == null) 
+            {
+                throw new NegocioException("No se proporciono una cuenta valida");
+            }
+
+            mensajeDAO.guardar(mensaje);
+
+        } 
+        catch (PersistenciaException e) 
+        {
+            System.out.println(e.getMessage());
+            throw new NegocioException(e.getMessage());
+        }
+        catch(NullPointerException e)
+        {
+            System.out.println(e.getMessage());
+            throw new NegocioException(e.getMessage());
+        }
+    }
+    
+}
